8ef351dfe2e5318cde30bcf9c8756aa266a44114
James Middleton <99860209+Bkjay08@users.noreply.github.com>
2024-03-22 16:10:47 -0400

    Create Hello.py
    Initial project commit

---
+++ Hello.py
@@ -0,0 +1,28 @@
+# NAME:         James Middleton
+# ASSIGNMENT:   Project 1
+# Hello World Project
+
+# Example
+def hello_world():
+    return "Hello!"
+print(hello_world)
+# 1
+
+def squared_sum(lst):
+
+    sum_of_squares = sum(x**2 for x in lst)
+
+    return sum_of_squares
+
+result = squared_sum([])
+print(result)
+
+# 2
+def mix(a, b):
+    return ""
+
+def main():
+    print("squared sum: ", squared_sum([-3, 4]))
+    print("mix:         ", mix("1234", "abcd") == "1a2b3c4d")
+
+main()
